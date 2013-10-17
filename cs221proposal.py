0539b0bb2c1d33f3a484766b50f350172fad666b
elloraisrani <ellora@stanford.edu>
2013-10-17 15:17:25 -0700

    Update cs221proposal.py

--- cs221proposal.py
+++ cs221proposal.py
@@ -3,11 +3,15 @@ import sys
 
 def import_data(filename):
 	data = []
+	business_categories = set([])
 	with open(filename) as f:
 		for line in f:
 			line_as_dict = json.loads(line)
 			if line_as_dict['type'] == 'business' and 'Stanford University' in line_as_dict['schools']:
 				data.append(line_as_dict)
+			if line_as_dict['type'] == 'business':
+				for item in line_as_dict['categories']:
+					business_categories.add(item)
 	return data
 
 def write_list_to_file(json_as_list, stanford_file):
@@ -42,7 +46,7 @@ def divide_area(min_latitute, max_latitute, min_longitude, max_longitude):
 	return areas
 
 def parse():
-	json_as_list = import_data('yelp_academic_dataset.json')
+	(json_as_list, all_business_categories) = import_data('yelp_academic_dataset.json')
 	stanford_file = open('stanford_businesses.txt', 'w')
 	(min_latitute, max_latitute, min_longitude, max_longitude) = calculate_area_dimensions(json_as_list)
 	neighborhoods = divide_area(min_latitute, max_latitute, min_longitude, max_longitude)
